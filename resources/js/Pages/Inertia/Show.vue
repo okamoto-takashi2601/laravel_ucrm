ec5e6fb50acce52941bb484329af8777cee48b7a
okamoto-takashi <okamoto2601@gmail.com>
2023-04-12 22:03:44 +0900

    inertia-InertiaTest

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia'
+defineProps({
+    id : String,
+    blog : Object
+})
+const deleteConfirm = id =>{
+    // console.log(id)
+    Inertia.delete(`/inertia/show/${id}`,{
+        onBefore: ()=>confirm('本当に削除しますか？')
+    })
+}
+</script>
+
+<template>
+    <div>
+        {{ id }}<br>
+        {{ blog.title }} <br>
+        <button @click="deleteConfirm(blog.id)">削除</button>
+    </div>
+</template>
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,9 +1,26 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
+
+const newTitle = ref('')
+const newContent = ref('')
 
 </script>
 
 <template>
 <a href="/"> a tag</a>
-<Link href="/"> link tag</Link>
+<br>
+<Link href="/"> link 経由です</Link>
+<br>
+<Link :href="route('inertia.index')"> name route test</Link>
+<br>
+<Link :href="route('inertia.show',{id : 50})"> route parameter test</Link>
+<div class="mb-8"></div>
+<input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+<input type="text" name="newContent" v-model="newContent">{{ newContent }}<br>
+<Link as="button" method ="post" :href="route('inertia.store')" 
+:data="{
+    title : newTitle,
+    content : newContent
+}">DB保存テスト</Link>
 </template>
